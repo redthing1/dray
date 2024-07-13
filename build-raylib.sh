6873a35d2f2fa5b08368d8e13ad9dcb8d4991119
redthing1 <redthing1@alt.icu>
2024-07-12 20:34:46 -0700

    make dependency builder script more robust

--- build-raylib.sh
+++ build-raylib.sh
@@ -1,24 +1,54 @@
 #!/usr/bin/env bash
 
-DIR=$(dirname "$0")
-cd "$DIR"
-if [ ! -f libraylib.a ] || [ "$1" == "-f" ]; then
-    echo "[dray] Building Raylib static library..."
+###
+### DEPENDENCY LIBRARY BUILDER SCRIPT
+### redthing1
+###
 
-    rm -rf raylib_source
-    git clone https://github.com/xdrie/raylib.git raylib_source
-    cd raylib_source
+
+HOST="dray"
+LIB_NAME="raylib"
+SOURCETREE_URL="https://github.com/xdrie/raylib.git"
+SOURCETREE_DIR="raylib_source"
+LIB_FILE="libraylib.a"
+
+PACKAGE_DIR=$(dirname "$0")
+cd "$PACKAGE_DIR"
+pushd .
+if [ ! -f $LIB_FILE ] || [ "$1" == "-f" ]; then
+    echo "[$HOST] building $LIB_NAME library..."
+
+    # delete $SOURCETREE_DIR to force re-fetch source
+    if [ -d $SOURCETREE_DIR ] 
+    then
+        echo "[$HOST] source folder already exists, using it." 
+    else
+        echo "[$HOST] getting source to build $LIB_NAME" 
+        git clone $SOURCETREE_URL $SOURCETREE_DIR
+    fi
+
+    cd $SOURCETREE_DIR
     git checkout 4.0.0_patch
-
+    git pull
     git submodule update --init --recursive
+
+    echo "[$HOST] starting build of $LIB_NAME" 
+    #
+    # START BUILD
+    #
+    # build the library
     cd src/
     make -j$(nproc) PLATFORM=PLATFORM_DESKTOP RAYLIB_LIBTYPE=STATIC RAYLIB_MODULE_RAYGUI=TRUE
+    #
+    # END BUILD
+    #
 
-    pwd
-    cp libraylib.a ../../libraylib.a
-    cd ../..
-    # rm -rf raylib_source
+    echo "[$HOST] finished build of $LIB_NAME" 
+
+    echo "[$HOST] copying $LIB_NAME binary ($LIB_FILE)" 
+    cp -v $(pwd)/$LIB_FILE $PACKAGE_DIR/$LIB_FILE
+    popd
 else
-    # delete libraylib.a to force rebuild
-    echo "[dray] Raylib library already built."
+    # delete $LIB_FILE to force rebuild
+    echo "[$HOST] library $LIB_NAME already built."
 fi
