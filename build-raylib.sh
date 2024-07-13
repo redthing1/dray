ec43d7c0313866ba1be1d8b144c8267e8e73584e
redthing1 <redthing1@alt.icu>
2024-07-12 20:34:50 -0700

    update clib builder script

--- build-raylib.sh
+++ build-raylib.sh
@@ -5,17 +5,19 @@
 ### redthing1
 ###
 
+set -e
 
 HOST="dray"
 LIB_NAME="raylib"
-SOURCETREE_URL="https://github.com/redthing1/raylib.git"
+SOURCETREE_URL="https://github.com/redthing1/raylib"
 SOURCETREE_DIR="raylib_source"
-LIB_FILE="libraylib.a"
+SOURCETREE_BRANCH="4.2.0_patch"
+LIB_FILE_1="libraylib.a"
 
 PACKAGE_DIR=$(dirname "$0")
 cd "$PACKAGE_DIR"
 pushd .
-if [ ! -f $LIB_FILE ] || [ "$1" == "-f" ]; then
+if [ ! -f $LIB_FILE_1 ] || [ "$1" == "-f" ]; then
     echo "[$HOST] building $LIB_NAME library..."
 
     # delete $SOURCETREE_DIR to force re-fetch source
@@ -24,12 +26,11 @@ if [ ! -f $LIB_FILE ] || [ "$1" == "-f" ]; then
         echo "[$HOST] source folder already exists, using it." 
     else
         echo "[$HOST] getting source to build $LIB_NAME" 
-        git clone $SOURCETREE_URL $SOURCETREE_DIR
+        # git clone $SOURCETREE_URL $SOURCETREE_DIR
+        git clone --depth 1 --branch $SOURCETREE_BRANCH $SOURCETREE_URL $SOURCETREE_DIR
     fi
 
     cd $SOURCETREE_DIR
-    git checkout 4.2.0_patch
-    git pull
     git submodule update --init --recursive
 
     echo "[$HOST] starting build of $LIB_NAME" 
@@ -37,18 +38,18 @@ if [ ! -f $LIB_FILE ] || [ "$1" == "-f" ]; then
     # START BUILD
     #
     # build the library
-    cd src/
-    make -j$(nproc) PLATFORM=PLATFORM_DESKTOP RAYLIB_LIBTYPE=STATIC RAYLIB_MODULE_RAYGUI=TRUE
+    cd src
+    make -j$(nproc)
     #
     # END BUILD
     #
 
     echo "[$HOST] finished build of $LIB_NAME" 
 
-    echo "[$HOST] copying $LIB_NAME binary ($LIB_FILE)" 
-    cp -v $(pwd)/$LIB_FILE $PACKAGE_DIR/$LIB_FILE
+    echo "[$HOST] copying $LIB_NAME binary ($LIB_FILE_1) to $PACKAGE_DIR"
+    cp -v $(pwd)/$LIB_FILE_1 $PACKAGE_DIR/$LIB_FILE_1
     popd
 else
-    # delete $LIB_FILE to force rebuild
+    # delete $LIB_FILE_1 to force rebuild
     echo "[$HOST] library $LIB_NAME already built."
 fi
